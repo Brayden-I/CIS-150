5bf21f56b820ff76a294fd1dccd422a55ff0b45f
bi1012440 <bi1012440@ITLT17598>
2025-02-13 08:38:32 -0600

    Submission for Module 5: Catch Blocks

---
+++ Projects/CatchBlocks/CatchApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CatchApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "49"; // or use "49" in a notebook
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The age you have entered is not a valid format");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+        }
+    }
+}
